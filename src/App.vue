1121c2afa66eb49d7e1d3a1ad0aaf1632120c4ad
simonebonifazi <simonbonifraz@gmail.com>
2022-07-26 16:59:25 +0200

    base-structure

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+    <header> 
+      <figure>
+        <img src="" alt="logo">
+      </figure>
+    </header>
+    <main>
+      <div class="discs">
+        <div class="single-disc"></div>
+      </div>
+    </main>
   </div>
 </template>
 
