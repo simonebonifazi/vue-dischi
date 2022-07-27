d9308bd60e9095620f347de6b72c85d46077e740
simonebonifazi <simonbonifraz@gmail.com>
2022-07-27 20:23:43 +0200

    added-genreselector-component-

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -4,20 +4,16 @@
         <img src="https://th.bing.com/th/id/R.57dd160d1e8f82b1113ca3f73393621c?rik=EHp%2bG6a6uyaHaA&pid=ImgRaw&r=0" alt="logo">
       </figure>
 
-  
-    <select name="search-for-genre" id="genre-searcher" >
-        <option value="default">Seleziona genere</option>
-        <option value="jazz">jazz</option>
-        <option value="metal">metal</option>
-        <option value="rock">rock</option>
-        <option value="pop">pop</option>
-    </select>
+      <GenreSelector/>
+ 
   </header>
 </template>
 
 <script>
+import GenreSelector from './GenreSelector.vue'
 export default {
-name: "BaseHeader"
+name: "BaseHeader",
+components: {GenreSelector,},
 }
 </script>
 
@@ -34,10 +30,7 @@ header{
   figure{
     height: 100%;
   }
-  select{
-    padding: 5px;
-    border-radius: 5px;
-  }
+ 
 
 }
 </style>
---
+++ src/components/GenreSelector.vue
@@ -0,0 +1,23 @@
+<template>
+     <select name="search-for-genre" id="genre-searcher" >
+        <option value="default">Seleziona genere</option>
+        <option value="jazz">jazz</option>
+        <option value="metal">metal</option>
+        <option value="rock">rock</option>
+        <option value="pop">pop</option>
+    </select>
+</template>
+
+<script>
+export default {
+name: "GenreSelector",
+}
+</script>
+
+<style lang="scss" scoped>
+ select{
+    padding: 5px;
+    border-radius: 5px;
+  }
+
+</style>
